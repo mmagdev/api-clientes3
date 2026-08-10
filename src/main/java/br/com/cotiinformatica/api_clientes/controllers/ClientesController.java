42d8410b54f6cecef0204dc7209e720372867e6e
unknown <matheusmag.dev@gmail.com>
2026-08-10 12:54:09 -0300

    feat: adiciona config do banco de dados, connectionfactory, repositórios e consulta de planos n api

---
+++ src/main/java/br/com/cotiinformatica/api_clientes/controllers/ClientesController.java
@@ -0,0 +1,32 @@
+package br.com.cotiinformatica.api_clientes.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/clientes")
+public class ClientesController {
+
+    @PostMapping
+    public String post() {
+        return "Cliente cadastrado com sucesso!";
+    }
+
+    @PutMapping
+    public String put() {
+        return "Cliente atualizado com sucesso!";
+    }
+
+    @GetMapping
+    public String get() {
+        return "Consulta de clientes realizada com sucesso!";
+    }
+
+
+    @DeleteMapping
+    public String delete() {
+        return "Cliente excluído com sucesso.";
+    }
+
+
+}
---
+++ src/main/java/br/com/cotiinformatica/api_clientes/repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package br.com.cotiinformatica.api_clientes.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteRepository {
+}
